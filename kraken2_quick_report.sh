aa7f1826e4eb640eb36e300d7f39de1e0f94df85
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2021-10-17 01:41:49 -0300

    Update kraken2_quick_report.sh

--- kraken2_quick_report.sh
+++ kraken2_quick_report.sh
@@ -1,4 +1,22 @@
-# O arquivo taxin contém a lista de taxons de interesse
+#!/bin/bash
+
+# script: kraken2_quick_report.sh
+# autor: Luciano Kalabric <luciano.kalabric@fiocruz.br>
+# instituição: Oswaldo Cruz Foundation, Gonçalo Moniz Institute, Bahia, Brazil
+# objetivo: relatório resumido dos resultados da classificação taxonômica pelo kraken2
+# criação: 25 AGO 2021
+# ultima atualização: 17 OUT 2021
+# atualização: revisão do script
+# requisito: arquivo taxid que contém a lista de taxons de interesse
+
+# Valiação da entrada de dados na linha de comando
+if [[ $# -ne 2 ]]; then
+	echo "Faltando algum parâmetro!"
+	echo "Sintáxe: ./kraken2_quick_report.sh <BIBLIOTECA_MODEL> <BARCODE>"
+	echo "Exemplo: ./kraken2_quick_report.sh DENV_FTA_1_hac barcode01"
+	exit 0
+fi
+
 # Atualizar na medida do necessário
 RUNNAME=$1
 WF=31
@@ -6,12 +24,6 @@ BARCODE=$2
 RESULTSDIR="$HOME/ngs-analysis/$RUNNAME/wf$WF"
 READSLEVELDIR="${RESULTSDIR}/READS_LEVEL"
 
-if [[ $# -ne 2 ]]; then
-	echo "Faltando algum parâmetro!"
-	echo "Sintáxe: ./kraken2_quick_report.sh <BIBLIOTECA_MODEL> <BARCODE>"
-	echo "Exemplo: ./kraken2_quick_report.sh DENV_FTA_1_hac barcode01"
-	exit 0
-fi
 while read -r line ; do
 	count=$(agrep -q -w "$line" ${READSLEVELDIR}/${BARCODE}_report.txt | cut -f 2)
 	echo "$line - $count"
