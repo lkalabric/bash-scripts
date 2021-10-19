2d1a8a48c10229f60f48a5575de87a5d433733c5
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2021-10-19 20:21:28 -0300

    Update git_commands.sh

--- git_commands.sh
+++ git_commands.sh
@@ -5,29 +5,30 @@
 # instituição: Oswaldo Cruz Foundation, Gonçalo Moniz Institute, Bahia, Brazil
 # objetivo: uso do git para clonagem e controle de versão dos scripts
 # criação: 17 OUT 2021
-# ultima atualização: 17 OUT 2021
+# ultima atualização: 19 OUT 2021
 # atualização: criação deste script para aprender e lembrar os comandos git
 
 # GitHub CLI
 # Links: https://docs.github.com/pt/github-cli/github-cli/quickstart
 # https://docs.github.com/pt/repositories/creating-and-managing-repositories/cloning-a-repository
-# Clone remote to local
-#git clone https://github.com/lkalabric/bash-scripts.git /home/brazil1/bash-scripts
 
 # Define nome de usuário do Git
-#cd ~/bash-scripts
 #git config --global user.name "lkalabric"
 #git config --global user.email "luciano.kalabric@fiocruz.br"
 
-# Configura o método de autenticação do usuário
-#gh user lkalabric
+# Clone remote to local (apenas uma vez)
+#git clone https://github.com/lkalabric/ngs-scripts.git /home/brazil1/repos/ngs-scripts
 
 # Commit local
 #git commit -a -m "Atualização do script assembly.sh."
+#git branch -M main
 
 # Push changes to the server
-#git push
+#git push -u origin main
 
-# Conexão via SSH
-# Link: https://docs.github.com/pt/authentication/connecting-to-github-with-ssh/testing-your-ssh-connection
-#ssh -T git@github.com
+# Poll changes from server
+#git poll
+
+# Conexão Git
+# User: lkalabric
+# Password: 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
