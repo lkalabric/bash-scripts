d865abe1d6f7dddc4bb5bb30bbe753408b0dd0d4
lkalabric <luciano.kalabric@fiocruz.br>
2021-10-20 00:51:00 -0300

    Criação do arquivo .gitignore

--- header2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# script: git_ngs-scripts.sh
-# autor: Luciano Kalabric <luciano.kalabric@fiocruz.br>
-# instituição: Oswaldo Cruz Foundation, Gonçalo Moniz Institute, Bahia, Brazil
-# objetivo: uso do git para clonagem e controle de versão dos scripts
-# criação: 17 OUT 2021
-# ultima atualização: 19 OUT 2021
-# atualização: criação deste script para aprender e lembrar os comandos git
