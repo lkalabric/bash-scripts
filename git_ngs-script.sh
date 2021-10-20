85ca3b59abfd6310dc0d8b6edf9d8300d41f5aef
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2021-10-20 00:39:00 -0300

    Update git_ngs-script.sh

--- git_ngs-script.sh
+++ git_ngs-script.sh
@@ -10,29 +10,27 @@
 
 # GitHub CLI
 # Links: https://docs.github.com/pt/github-cli/github-cli/quickstart
-# https://docs.github.com/pt/repositories/creating-and-managing-repositories/cloning-a-repository
+# Remote repo URL https://docs.github.com/pt/repositories/creating-and-managing-repositories/cloning-a-repository
 
 # Quick setup — if you’ve done this kind of thing before
-# https://github.com/lkalabric/ngs-scripts.git
+# Remoto repo URL https://github.com/lkalabric/ngs-scripts.git
 
 # Configure git local
-#git config --global user.name "lkalabric"
-#git config --global user.email "luciano.kalabric@fiocruz.br"
+# git config --global user.name "lkalabric"
+# git config --global user.email "luciano.kalabric@fiocruz.br"
 
 # Clone remote to local (run only once)
-#git clone https://github.com/lkalabric/ngs-scripts.git /home/brazil1/repos/ngs-scripts
+# git clone https://github.com/lkalabric/ngs-scripts.git /home/brazil1/repos/ngs-scripts
 
-# Commit local
-#git commit -a -m "Atualização do script assembly.sh."
-#git branch -M main
+# Commit
+# git add -A
+# git commit -m "Atualização do script..."
 
-# Push changes to the server
-#git push -u origin main
+# Pull/Push changes to the server
+# git pull
+# git push -u origin main
 
 # Git auth
 # User: lkalabric
 # Scopes: repo; admin:org; user
-# Password=personal token: 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
-
-# Pull changes from server
-#git pull
+# Password=personal token: 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
