2120bd8fb15a6c550b0a2de4107c82f92278f77e
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2021-11-23 17:54:26 -0300

    Update guppy_gpu_v1_ag.sh
    Salva os resultados na pasta RUNNAME_MODELO

--- guppy_gpu_v1_ag.sh
+++ guppy_gpu_v1_ag.sh
@@ -7,8 +7,10 @@
 
 # Nome dos dados passado na linha de comando
 RUNNAME=$1 
+MODELO=$2
+
 if [[ $# -eq 0 ]]; then
-	echo "Falta o nome dos dados!"
+	echo "Falta o nome dos dados e ou o modelo de análise!"
 	exit 0
 fi
 # Caminho de INPUT dos dados fast5
@@ -19,19 +21,17 @@ fi
  	exit 0
  fi
 # Caminho de OUTPUT dos resultados da análise NGS
-mkdir -p "${HOME}/ngs-analysis/${RUNNAME}_bm"
-SAVE_DIR="${HOME}/ngs-analysis/${RUNNAME}_bm"
+mkdir -p "${HOME}/ngs-analysis/${RUNNAME}_${MODELO}"
+SAVE_DIR="${HOME}/ngs-analysis/${RUNNAME}_${MODELO}"
 # Flow cell and kit OR config file
 # FLOWCELL="FLO-MIN106"
 # KIT="SQK-LSK109"
 # Arquivo _fast para benckmark apenas e _hac para as análises
-CONFIG="dna_r9.4.1_450bps_fast.cfg" # "dna_r9.4.1_450bps_hac.cfg"
+# CONFIG="dna_r9.4.1_450bps_fast.cfg" # "dna_r9.4.1_450bps_hac.cfg"
+CONFIG="dna_r9.4.1_450bps_${MODELO}.cfg"
 
 # Parametros para otimização da GPU
 # --gpu_runners_per_device, --chunk_size, --chunks_per_runner, -x
 
 # Comando para guppy_basecaller usando GPU do LAPTOP-Yale e parâmetros otimizados a partir do benchmark 
 guppy_basecaller -r -i ${INPUT_DIR} -s ${SAVE_DIR} -c ${CONFIG} -x auto --gpu_runners_per_device 4 --chunk_size 1000 --chunks_per_runner 50 --verbose_logs
-
-
-
