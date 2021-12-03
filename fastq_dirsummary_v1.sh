4ae5a388471d5fabd91e8813c3b427955307bb23
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2021-12-03 10:12:16 -0300

    Update fastq_dirsummary_v1.sh

--- fastq_dirsummary_v1.sh
+++ fastq_dirsummary_v1.sh
@@ -4,7 +4,8 @@
 # Autor: Luciano Kalabric
 # Conta os arquivos .fastq do diretório barcode_dir
 
-ls $1/*.fastq | wc -l
+find -H $1 -name *.fastq | wc -l
 # Conta o total de sequencias do diretório barcode_dir
-grep -c "runid" $1/*.fastq | cut -d : -f 2 | awk '{s+=$1} END {printf "%.0f\n",s}'
+grep -o "runid" $(find -H  $1 -name *.fastq) | wc -l
+# grep -c "runid" $1/*.fastq | cut -d : -f 2 | awk '{s+=$1} END {printf "%.0f\n",s}'
 
