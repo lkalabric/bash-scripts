cd0afdc4afc1a9124deedc0b9c48a3345c55c3f7
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2022-03-18 09:58:34 -0300

    Update assembly2.sh

--- assembly2.sh
+++ assembly2.sh
@@ -23,8 +23,8 @@ REFGENDIR="${HOME}/data/REFGEN"
 # Caminhos de OUTPUT das análises
 RESULTSDIR="${HOME}/ngs-analysis/${RUNNAME}_${MODEL}"
 READSLEVELDIR="${RESULTSDIR}/wf${WF}/READS_LEVEL"
-CONTIGSLEVELDIR="${RESULTSDIR}/wf${WF}/CONTIGS_LEVEL"
-[ ! -d $CONTIGSLEVELDIR ] && mkdir -vp $CONTIGSLEVELDIR
+ASSEMBLYDIR="${RESULTSDIR}/wf${WF}/ASSEMBLY"
+[ ! -d $ASSEMBLYDIR ] && mkdir -vp $ASSEMBLYDIR
 
 # Parâmetro de otimização das análises por CPU
 THREADS="$(lscpu | grep 'CPU(s):' | awk '{print $2}' | sed -n '1p')"
@@ -46,10 +46,10 @@ source activate ngs
 for j in $(find ${REFGENDIR} -type f -name "*.mmi" | while read o; do basename $o | cut -d. -f1; done | sort | uniq); do
 	for i in $(find ${READSLEVELDIR} -type f -name "*.fasta" | while read o; do basename $o | cut -d. -f1; done | sort | uniq); do
 		echo "Mapeando ${READSLEVELDIR}/${i}.corrected.fasta..."
-		minimap2 -t ${THREADS} -ax map-ont ${REFGENDIR}/${j}.mmi ${READSLEVELDIR}/${i}.corrected.fasta | samtools sort -@ ${THREADS} -o ${CONTIGSLEVELDIR}/${i}.${j}.sorted.bam -	
-		samtools view -@ ${THREADS} -h -F 4 -b ${CONTIGSLEVELDIR}/${i}.${j}.sorted.bam > ${CONTIGSLEVELDIR}/${i}.${j}.sorted.mapped.bam
-		samtools index -@ ${THREADS} ${CONTIGSLEVELDIR}/${i}.${j}.sorted.mapped.bam
-		samtools mpileup -A -d 0 -Q 0 ${CONTIGSLEVELDIR}/${i}.${j}.sorted.mapped.bam | ivar consensus -p ${CONTIGSLEVELDIR}/${i}.${j}
+		minimap2 -t ${THREADS} -ax map-ont ${REFGENDIR}/${j}.mmi ${READSLEVELDIR}/${i}.corrected.fasta | samtools sort -@ ${THREADS} -o ${ASSEMBLYDIR}/${i}.${j}.sorted.bam -	
+		samtools view -@ ${THREADS} -h -F 4 -b ${ASSEMBLYDIR}/${i}.${j}.sorted.bam > ${ASSEMBLYDIR}/${i}.${j}.sorted.mapped.bam
+		samtools index -@ ${THREADS} ${ASSEMBLYDIR}/${i}.${j}.sorted.mapped.bam
+		samtools mpileup -A -d 0 -Q 0 ${ASSEMBLYDIR}/${i}.${j}.sorted.mapped.bam | ivar consensus -p ${ASSEMBLYDIR}/${i}.${j}
 	done
 done
 
