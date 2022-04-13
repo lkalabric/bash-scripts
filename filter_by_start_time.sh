22a3dfc399884c0d78948dcc1c19f30968b206f3
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2022-04-13 14:21:17 -0300

    Update filter_by_start_time.sh

--- filter_by_start_time.sh
+++ filter_by_start_time.sh
@@ -10,6 +10,7 @@
 # Validação da entrada de dados na linha de comando
 RUNNAME=$1 	# Nome do dado passado na linha de comando
 MODEL=$2	# Modelo de basecalling fast hac sup
+STAR_DATE=#3	# Data da corrida
 
 if [[ $# -eq 0 ]]; then
 	echo "Falta o nome dos dados, número do worflow ou modelo Guppy Basecaller!"
@@ -23,12 +24,12 @@ FILTER_BY_START_TIMEDIR="${RESULTSDIR}/FILTER_BY_START_TIME"
 
 # Filter_by_start_time
 # Tempos para análise
-declare -a START_TIME=(1 2 4 12 24 72)
+declare -a START_TIME=(01 02 04 12 24 72)
 echo -e "\nExecutando filter_by_start_time..."
 for i in $(find ${DEMUXCATDIR} -type f -exec basename {} .fastq \;); do
   wc -l "${DEMUXCATDIR}/${i}.fastq"
   for j in "${START_TIME[@]}" do
-    grep -A3 "2021-06-02T${j}" "${DEMUXCATDIR}/${i}.fastq" > "${FILTER_BY_START_TIMEDIR}/${i}.${j}.fastq"
+    grep -A3 "${START_DATE}T${j}" "${DEMUXCATDIR}/${i}.fastq" > "${FILTER_BY_START_TIMEDIR}/${i}.${j}.fastq"
     wc -l "${FILTER_BY_START_TIMEDIR}/${i}.${j}.fastq"
   done
  done
