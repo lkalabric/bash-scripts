87dc9c17f13bf2d6054ac1237255cb172c51f7ce
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2022-04-13 15:57:31 -0300

    Update filter_by_start_time.sh

--- filter_by_start_time.sh
+++ filter_by_start_time.sh
@@ -26,20 +26,18 @@ FILTER_BY_START_TIMEDIR="${RESULTSDIR}/FILTER_BY_START_TIME"
 [ ! -d "${FILTER_BY_START_TIMEDIR}" ] && mkdir -vp "${FILTER_BY_START_TIMEDIR}"
 
 # Filter_by_start_time
-# Tempos para análise
-declare -a START_TIME=(01 02 04 12 24 72)
+# Expressões regulares para filtar pelo tempo da corrida
+declare -a START_TIME=(01 02 04 12 24 all)
+declare -a REGEXP=("0[0-1]" "0[0-2]" "0[0-4]" "0[0-9]|1[0-2]" "0[0-9]|1[0-9]|2[0-4]" "..")
 for i in $(find ${DEMUXCATDIR} -type f -exec basename {} .fastq \; | sort); do
 	echo -e "\nContando as reads do arquivo ${DEMUXCATDIR}/${i}.fastq..."
 	wc -l "${DEMUXCATDIR}/${i}.fastq"
 	read -p "Pressione qualquer tecla para continuar..."
-	for j in "${START_TIME[@]}"; do
+	for ((j=1; j<=6; j++)); do
 		echo -e "\nExecutando filter_by_start_time ${j}..."
-		echo -e "${START_DATE}T${j}"
-		echo -e "${DEMUXCATDIR}/${i}.fastq"
-		echo -e "${FILTER_BY_START_TIMEDIR}/${i}.${j}.fastq"
-		grep -A3 "${START_DATE}T${j}" "${DEMUXCATDIR}/${i}.fastq" > "${FILTER_BY_START_TIMEDIR}/${i}.${j}.fastq"
-		echo -e "\nContando as reads do arquivo "${FILTER_BY_START_TIMEDIR}/${i}.${j}.fastq"..."
-		wc -l "${FILTER_BY_START_TIMEDIR}/${i}.${j}.fastq"
+		egrep -A3 "${START_DATE}T${REGEXP[$j]}" "${DEMUXCATDIR}/${i}.fastq" > "${FILTER_BY_START_TIMEDIR}/${i}.${STAR_TIME[$j]}.fastq"
+		echo -e "\nContando as reads do arquivo "${FILTER_BY_START_TIMEDIR}/${i}.${STAR_TIME[$j]}.fastq"..."
+		wc -l "${FILTER_BY_START_TIMEDIR}/${i}.${STAR_TIME[$j]}.fastq"
 	done
 done
 
