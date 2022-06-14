52f74ff1f1f6a0346875417c70044e808d156680
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2022-06-14 12:14:46 -0300

    Update kraken2_quick_report.sh

--- kraken2_quick_report.sh
+++ kraken2_quick_report.sh
@@ -10,21 +10,41 @@
 # requisito: arquivo taxin que contém a lista de taxons de interesse
 
 # Valiação da entrada de dados na linha de comando
-if [[ $# -ne 2 ]]; then
-	echo "Faltando algum parâmetro!"
-	echo "Sintáxe: ./kraken2_quick_report.sh <BIBLIOTECA_MODEL> <BARCODE>"
-	echo "Exemplo: ./kraken2_quick_report.sh DENV_FTA_1_hac barcode01"
-	exit 0
-fi
+die () {
+    echo >&2 "$@"
+    exit 0
+}
 
-# Atualizar na medida do necessário
-RUNNAME=$1
-WF=31
-BARCODE=$2
-RESULTSDIR="$HOME/ngs-analysis/$RUNNAME/wf$WF"
-READSLEVELDIR="${RESULTSDIR}/READS_LEVEL"
+case $# in
+	1)
+		if [ ! -f $1 ]; then
+			echo "Arquivo ou diretório inválido"
+			echo "Sintáxe: ./kraken2_quick_report.sh <caminho_completo/barcode01.blastn>"
+			echo "Exemplo: ./kraken2_quick_report.sh ngs-library/DENV_FTA_1_fast/wf3/READS_LEVEL/barcode01_report.txt"
+			exit 1
+		fi
+		$FILENAME=$1
+		
+	;;
+	2)
+		RUNNAME=$1
+		BARCODE=$2
+		RESULTSDIR="${HOME}/ngs-analysis/${RUNNAME}"
+		READSLEVELDIR="${RESULTSDIR}/wf3/READS_LEVEL"
+		FILENAME="${READSLEVELDIR}/${BARCODE}_report.txt"
+		if [ ! -f ${FILENAME} ]; then	
+			echo "Falta o nome da biblioteca_model e/ou do barcodeXX"
+			echo "Sintáxe: ./kraken2_quick_report.sh <BIBLIOTECA_MODEL> <BARCODE>"
+			echo "Exemplo: ./kraken2_quick_report.sh DENV_FTA_1_hac barcode01"
+			exit 2
+		fi
+		
+	*)
+		die "Mínimo de 1 e máximo de 2 argumentos são requiridos, $# provido"
+	;;
+esac
 
 while read -r line ; do
-	count=$(agrep -q -w "$line" ${READSLEVELDIR}/${BARCODE}_report.txt | cut -f 2)
+	count=$(agrep -q -w "$line" ${FILENAME} | cut -f 2)
 	echo "$line - $count"
 done < <(cat /home/brazil1/data/REFSEQ/taxin | tr '\t' ';')
