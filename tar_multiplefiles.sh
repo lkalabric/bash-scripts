2b785f0335add7321a4352530fc6c33bb1c9f1b2
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2022-07-08 09:20:03 -0300

    Update tar_multiplefiles.sh

--- tar_multiplefiles.sh
+++ tar_multiplefiles.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+# script: tar_multiplefiles.sh
+# autor: Luciano Kalabric <luciano.kalabric@fiocruz.br>
+# instituição: Oswaldo Cruz Foundation, Gonçalo Moniz Institute, Bahia, Brazil
+# objetivo: compacta multiplos arquivos de um diretório em multiplos arquivos .tar
+# criação: 08 JUL 2022
+# ultima atualização: 08 JUL 2022
+# atualização: criação do arquivo
+
 for file in `ls $1`; do tar -czvf $file.tar.gz $file ; done
