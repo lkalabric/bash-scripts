a19fa254e01de73c03d691bd0ac253a08f3c4f03
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2022-10-07 15:02:18 -0500

    Update fastq_dirsummary.sh

--- fastq_dirsummary.sh
+++ fastq_dirsummary.sh
@@ -5,13 +5,16 @@
 # Conta os arquivos .fastq do diretório barcode_dir
 
 # Conta o total de arquivos .fastq do diretório
+echo "Número de arquivos .fastq:"
 find -H $1 -name *.fastq | wc -l
 
 # Conta o total de reads do diretório
+echo "Total reads"
 grep -o "runid" $(find -H  $1 -name *.fastq) | wc -l
 # grep -c "runid" $1/*.fastq | cut -d : -f 2 | awk '{s+=$1} END {printf "%.0f\n",s}'
 
 # Conta o total de reads por BC
+echo "Total reads por BC:"
 for i in $(find $1/*.fastq -type f -exec basename {} .fastq \; | cut -d_ -f1 | sort); do
-	grep -o "runid" $1/${i}.fastq | wc -l
+	echo "${i} - $(grep -o "runid" $1/${i}.fastq | wc -l)"
 done
