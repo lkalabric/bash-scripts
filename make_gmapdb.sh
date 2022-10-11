88805a845d4a33017de62b4ca27e60574d19c5a2
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2022-10-11 11:59:34 -0500

    Update make_gmapdb.sh

--- make_gmapdb.sh
+++ make_gmapdb.sh
@@ -7,23 +7,22 @@
 # Referencia: https://github.com/juliangehring/GMAP-GSNAP/blob/master/README
 
 # Validação da entrada de dados na linha de comando
-GENOMA=$1 	  # Nome do genoma
-FASTA=$2      # Arquivo FASTA
+GENOMA=$1 	# Nome do genoma
+FASTA=$2      	# Arquivo FASTA
+FLAGS=$3	# Flags, se aplicável
 if [[ $# -eq 0 ]]; then
 	echo "Falta o nome do genoma referência ou do arquivo(s) FASTA(s)!"
 	echo "Sintáxe: ./make_gmapdb.sh <GENOMA> <FASTA>"
-  echo "Exemplo: ./make_gmapdb.sh GRCh38 GRCh38.p13.genome.fa.gz
+  	echo "Exemplo: ./make_gmapdb.sh GRCh38 data/GRCh38/GRCh38.p13.genome.fa.gz -g"
+  	echo "Exemplo: ./make_gmapdb.sh HCV data/REFGEN/hcv1a.fasta"
 	exit 0
 fi
-
-# Diretórios de dados
-FASTADIR=${HOME}/data/GENOMA/${1}
 
 # Diretório do banco de dados 
 GMAPDBDIR=${HOME}/data/GMAPDB
 
 # Reseta o diretório antes de criar um novo banco de dados
-[[ -d ${GMAPDBDIR}/${1} ]] && rm -r ${GMAPDBDIR}/${1}
+[[ -d ${GMAPDBDIR}/${GENOMA} ]] && rm -r ${GMAPDBDIR}/${GENOMA}
 
 # Monta o banco de dados
-gmap_build -d $1 ${FASTADIR}/GRCh38.p13.genome.fa.gz -g
+gmap_build -d ${GENOMA} ${FASTA} ${FLAGS}
