0c65697787ae27f5b29c6a28efcb5db0e1213216
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2022-10-12 10:50:26 -0500

    Update header.sh

--- header.sh
+++ header.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# script: git_ngs-scripts.sh
+# script: header.sh
 # autor: Luciano Kalabric <luciano.kalabric@fiocruz.br>
 # instituição: Oswaldo Cruz Foundation, Gonçalo Moniz Institute, Bahia, Brazil
 # objetivo: uso do git para clonagem e controle de versão dos scripts
 # criação: 17 OUT 2021
 # ultima atualização: 19 OUT 2021
-# atualização: criação deste script para aprender e lembrar os comandos git
+# atualização: cabeçalho padrão utilizado nos scripts criados neste projeto
