9c4bc84f26175713815366fcccccb379831b3643
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2022-10-12 10:50:55 -0500

    Update header.sh

--- header.sh
+++ header.sh
@@ -5,5 +5,5 @@
 # instituição: Oswaldo Cruz Foundation, Gonçalo Moniz Institute, Bahia, Brazil
 # objetivo: uso do git para clonagem e controle de versão dos scripts
 # criação: 17 OUT 2021
-# ultima atualização: 19 OUT 2021
+# ultima atualização: 12 OUT 2022
 # atualização: cabeçalho padrão utilizado nos scripts criados neste projeto
