a6f5b163b98635f0ae124599841c94a6ea51f3a3
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2022-10-12 09:34:50 -0500

    Update make_refgen.sh

--- make_refgen.sh
+++ make_refgen.sh
@@ -9,7 +9,7 @@
 
 REFSEQDIR="${HOME}/data/REFSEQ"
 
-# Extrai os genomas referencia do arquivo refser.fasta a partir da lista disponível em refgen
+# Extrai os genomas referencia do arquivo refseq.fasta a partir da lista disponível em refgen
 # Link: https://www.biostars.org/p/319099/
 cat ${REFSEQDIR}/refgen | cut -f 2 | awk '{gsub("_","\\_",$0);$0="(?s)^>"$0".*?(?=\\n(\\z|>))"}1' | pcregrep -oM -f - ${REFSEQDIR}/refseq.fasta > ${REFSEQDIR}/refgen.fasta
 # grep -A 1 -wFf $(cut ${REFSEQDIR}/refgen -f 2) ${REFSEQDIR}/refseq.fasta > ${REFSEQDIR}/refgen.fasta
