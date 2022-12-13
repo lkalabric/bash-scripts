c2f008906a74dccea2f06fa1f8a698667c0e2d7a
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2022-12-13 14:42:54 -0300

    Update metagenomics6.sh

--- metagenomics6.sh
+++ metagenomics6.sh
@@ -83,6 +83,7 @@ RESULTSDIR="${HOME}/ngs-analysis/${RUNNAME}_${MODEL}/wf${WF}"
 BASECALLDIR="${RESULTSDIR}/BASECALL"
 DEMUXDIR="${RESULTSDIR}/DEMUX"
 DEMUXCATDIR="${RESULTSDIR}/DEMUX_CAT"
+FILTERBYSTARTTIMEDIR="${RESULTSDIR}/FILTERBYSTARTTIME"
 QCFILTERSDIR="${RESULTSDIR}/QC_FILTERS"
 CUTADAPTDIR="${RESULTSDIR}/CUTADAPT"
 NANOFILTDIR="${RESULTSDIR}/NANOFILT"
@@ -215,36 +216,27 @@ function demux_headcrop () {
 
 
 function filter_by_start_time () {
-  # script: filter_by_star_time.sh
-  # autor: Luciano Kalabric <luciano.kalabric@fiocruz.br>
-  # instituição: Oswaldo Cruz Foundation, Gonçalo Moniz Institute, Bahia, Brazil
-  # criação: 13 ABR 2022
-  # última atualização: 02 DEZ 2022
-  # versão 2: 
-  # link: https://www.biostars.org/p/389679/
-
-  # Caminhos de INPUT das análises
-  RESULTSDIR="${HOME}/ngs-analysis/${RUNNAME}_${MODEL}"
-  DEMUXCATDIR="${RESULTSDIR}/wf${WF}/DEMUX_CAT"
-
-  # Caminho de OUTPUT das análises
-  FILTER_BY_START_TIMEDIR="${RESULTSDIR}/wf${WF}_filter/DEMUX_CAT"
-  [ ! -d "${FILTER_BY_START_TIMEDIR}" ] && mkdir -vp "${FILTER_BY_START_TIMEDIR}"
-
-  # Define os STAR_TIME de interesse
-  declare -a START_TIME=(01 02 04 08 12 16 24 all)
-  # Define as expressões regulares de cada START_TIME para busca
-  declare -a REGEXP=("(0+[0-1])" "(0+[0-2])" "(0+[0-4])" "(0+[0-8])" "(0+[0-9]|1+[0-2])" "(0+[0-9]|1+[0-6])" "(0+[0-9]|1+[0-9]|2+[0-4])" "..")
-  for i in $(find "${IODIR}"/*.fastq -type f -exec basename {} .fastq \; | sort); do
-    echo -e "\nContando as reads do arquivo ${DEMUXCATDIR}/${i}.fastq..."
-    grep "${RUNNAME}" "${DEMUXCATDIR}/${i}.fastq" | wc -l
-    for ((j=0; j<=7; j++)); do
-      echo -e "\nExecutando filter_by_start_time ${START_TIME[${j}]}..."
-      egrep -A3 "start_time=..........T${REGEXP[${j}]}" "${DEMUXCATDIR}/${i}.fastq" > "${FILTER_BY_START_TIMEDIR}/${i}_${START_TIME[${j}]}.fastq"
-      echo -e "\nContando as reads do arquivo "${FILTER_BY_START_TIMEDIR}/${i}_${START_TIME[${j}]}.fastq"..."
-      grep "${RUNNAME}" "${FILTER_BY_START_TIMEDIR}/${i}_${START_TIME[${j}]}.fastq" | wc -l
-    done
-  done
+	if [ ! -d "${FILTERBYSTARTTIMEDIR}" ]; then
+		mkdir -vp "${FILTERBYSTARTTIMEDIR}"
+		# Define os STAR_TIME de interesse
+		declare -a START_TIME=(01 02 04 08 12 16 24 all)
+		# Define as expressões regulares de cada START_TIME para busca
+		declare -a REGEXP=("(0+[0-1])" "(0+[0-2])" "(0+[0-4])" "(0+[0-8])" "(0+[0-9]|1+[0-2])" "(0+[0-9]|1+[0-6])" "(0+[0-9]|1+[0-9]|2+[0-4])" "..")
+		for i in $(find "${IODIR}"/*.fastq -type f -exec basename {} .fastq \; | sort); do
+	    		echo -e "\nContando as reads do arquivo ${DEMUXCATDIR}/${i}.fastq..."
+			grep "${RUNNAME}" "${DEMUXCATDIR}/${i}.fastq" | wc -l >> ${FILTERBYSTARTTIMEDIR}/${i}_filterlog.txt
+			for ((j=0; j<=7; j++)); do
+				echo -e "\nExecutando filter_by_start_time ${START_TIME[${j}]}..."
+				# Filtar as reads por start_time
+				egrep -A3 "start_time=..........T${REGEXP[${j}]}" "${DEMUXCATDIR}/${i}.fastq" > "${FILTER_BY_START_TIMEDIR}/${i}_${START_TIME[${j}]}.fastq"
+				echo -e "\nContando as reads do arquivo "${FILTER_BY_START_TIMEDIR}/${i}_${START_TIME[${j}]}.fastq"..."
+				grep "${RUNNAME}" "${FILTER_BY_START_TIMEDIR}/${i}_${START_TIME[${j}]}.fastq" | wc -l >> ${FILTERBYSTARTTIMEDIR}/${i}_filterlog.txt
+			done
+		done
+	else
+		echo "Usando dados FILTERBYSTARTTIME analisados previamente..."
+	fi
+	IODIR=$FILTERBYSTARTTIMEDIR
 }
 
 
