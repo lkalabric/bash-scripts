b941891837cb5d226dbb207dbdbe5448e59c0077
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-01-19 13:57:24 -0300

    Update metagenomics7.sh

--- metagenomics7.sh
+++ metagenomics7.sh
@@ -187,7 +187,7 @@ function demux () {
 		[ ! -d ${DEMUXCATDIR} ] && mkdir -vp ${DEMUXCATDIR}
 		for i in $(find ${DEMUXDIR} -mindepth 1 -type d -name "barcode*" -exec basename {} \; | sort); do
 			[ -d "${DEMUXDIR}/${i}" ] && cat ${DEMUXDIR}/${i}/*.fastq > "${DEMUXCATDIR}/${i}.fastq"
-			grep -c "runid" ${DEMUXCATDIR}/${i}.fastq >> ${DEMUXCATDIR}/passed_reads.log
+			echo "${i} $(grep -c "runid" ${DEMUXCATDIR}/${i}.fastq)" >> ${DEMUXCATDIR}/passed_reads.log
 		done
 	else
 		echo "Usando dados DEMUX_CAT analisados previamente..."
